33040721db543b3d937bc3296d38fcfc2a8bdc8f
rakaardiansyah <raka.ardiansyah23@gmail.com>
2023-11-17 23:24:35 +0700

    penyesuaian dengan di figma

--- src/app/auth-screens/signup/signup.page.ts
+++ src/app/auth-screens/signup/signup.page.ts
@@ -15,14 +15,12 @@ export class SignupPage implements OnInit {
   isLoading!: boolean;
 
   isChecked: boolean = false;
-  checkboxLabel: string = 'Bermaksud untuk menjadi anggota koperasi dan sebagai anggota koperasi saya bersedia untuk menjalankan segala aturan yang tertuang dalam Anggaran Dasar, Anggaran Rumah Tangga Koperasi dan peraturan lain yang terkait.';
-  checkboxLabel2: string = 'Dan sekaligus memberikan kuasa kepada Kopkar BRIS untuk memotong Gaji terkait kewajiban Anggota melalui HCS';
+  checkboxLabel: string = 'Lorem ipsum dolor sit amet consectetur dapibus faucibus fusce euismod odio eu mauris neque ultrices bibendum non posuere morbi urna condimentum Hac ultricies tellus sed aliquam';
 
   
   onFileSelected(event: any) {
     const selectedFile = event.target.files[0];
     console.log('Selected File:', selectedFile);
-    // You can perform further actions with the selected file here
   }
 
   constructor(
@@ -39,25 +37,10 @@ export class SignupPage implements OnInit {
       noktp: new FormControl(null, {validators: [Validators.required]}),
       tanggal: new FormControl(null, {validators: [Validators.required]}),
       jenkel: new FormControl(null, {validators: [Validators.required]}),
-      statuspegawai: new FormControl(null, {validators: [Validators.required]}),
-      unitkerja: new FormControl(null, {validators: [Validators.required]}),
-      jabatan: new FormControl(null, {validators: [Validators.required]}),
-      rekpayroll: new FormControl(null, {validators: [Validators.required]}),
-      emailluar: new FormControl(null, {validators: [Validators.required]}),
       nohp: new FormControl(null, {validators: [Validators.required]}),
-      tabexlegacy: new FormControl(null, {validators: [Validators.required]}),
-      
-      upld_ktp: new FormControl(null, {validators: [Validators.required]}),
-      upld_idcard: new FormControl(null, {validators: [Validators.required]}),
-      upld_slipgaji: new FormControl(null, {validators: [Validators.required]}),
-
-
       cbox_ket1: new FormControl(null, {validators: [Validators.required]}),
-      cbox_ket2: new FormControl(null, {validators: [Validators.required]}),
-
-
-      // password: new FormControl(null, {validators: [Validators.required, Validators.minLength(8)]}),
-      // confirm_password: new FormControl(null, {validators: [Validators.required, Validators.minLength(8)]})
+      password: new FormControl(null, {validators: [Validators.required, Validators.minLength(8)]}),
+      confirm_password: new FormControl(null, {validators: [Validators.required, Validators.minLength(8)]})
     });
   }
 
@@ -80,14 +63,11 @@ export class SignupPage implements OnInit {
 
   toggleCheckbox() {
     this.isChecked = !this.isChecked;
-    // Tambahkan logika lain yang diperlukan saat checkbox diubah
-    // Contohnya bisa menampilkan pesan atau melakukan tindakan tertentu.
+
     if (this.isChecked) {
       console.log('Checkbox checked');
-      // Lakukan tindakan lainnya jika checkbox dicentang
     } else {
       console.log('Checkbox unchecked');
-      // Lakukan tindakan lainnya jika checkbox tidak dicentang
     }
   }
 
